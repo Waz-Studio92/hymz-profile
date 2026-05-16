b0595a7f3c8c6c924a47213711f55ae64b7a7007
Kohei Yoshida <was914hs@gmail.com>
2026-05-16 22:22:54 +0900

    仕様を大幅に変更

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,15 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('sidebar.html')
-        .then(Response => Response.text ())
-        .then(data => {
-            document.querySelector('#sidebar').innerHTML = data;
-        })
-        .catch(error => console.error('Error Loading sidebar', error));
+function createContent(item) {
+    // tag,text,childrenを除いた「残りの属性(id,hrf,hrefなど)」を attrs に纏める
+    const { tag, text, children, id, type, class: className, for: htmlFor, href, src, art, target, url, ...attrs} = item;
+    const el = document.createElement(tag);
+
+    Object.assign(el, attrs);
+    // class: classNameに変換
+    if (className) el.className = className;
+    if (htmlFor) el.htmlFor = htmlFor;
+    
+    // テキストがあれば挿入
+    if(text) el.textContent = text;
+
+    if(children) {
+        children.forEach(child => el.append(createContent(child)));
+    }
+    return el;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const configResponse = await fetch ('./assets/data/api.json');
+        if (!configResponse.ok) throw new Error ('Config load failed');
+        const config = await configResponse.json();
+
+        // JSON "parts": の設定通りにforEachで処理
+        config.parts.forEach(part => {
         
-    fetch('footer.html')
-        .then(Response => Response.text ())
-        .then(data => {
-            document.querySelector('#footer').innerHTML = data;
-        })
-        .catch(error => console.error('Error Loading footer', error));
+            // #sidebar から[#]を消してID名に変換
+            const contentContainerId = part.id.replace('#', '');
+            const container = document.getElementById(contentContainerId);
+
+            //part.id に対応するデータ(例: "sidebar" = "sideContent")を推測して取得
+            const dataKey = part.dataKey;
+            let dataList = config[dataKey];
+
+            if (container && dataList) {
+                dataList.forEach(data => container.appendChild(createContent(data)));
+            }
+
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
 });
